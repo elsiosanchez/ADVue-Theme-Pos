38d4fb7af1e381da6948dee8a2e98a7cf938996d
elsiosanchez <elsiosanches@gmail.com>
2022-04-07 16:57:10 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# ADVue-Theme-Pos
-Point of Sale Theme for ADempiere Vue
+<p align="center">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Adempiere-logo.png" />
+</p>
+
+# ADVue-Theme-Pos - Theme based on [Vue Element Admin](https://panjiachen.github.io/vue-element-admin-site) for [ADempiere Vue](https://github.com/adempiere/adempiere-vue)
+
+New ADVue-Theme-Pos template theme for [ADempiere Vue](https://github.com/adempiere/adempiere-vue), based on [Vue Element Admin](https://panjiachen.github.io/vue-element-admin-site/). 90% of the ADVue-Theme-Pos code is locked in the [Vue Element Admin](https://panjiachen.github.io/vue-element-admin-site) library. Thanks to this, ADVue-Theme-Pos can keep its flexibility, but minimize the risk of errors.
+
+## 🔌  Installation
+In order to use the new ADVue Theme Pos theme in your [ADempiere Vue](https://github.com/adempiere/adempiere-vue) installation, you need:
+
+1. Install `lerna` globally:
+
+   ```
+   npm install -g lerna
+   ```
+
+   or
+
+   ```
+   yarn global add lerna
+   ```
+
+1. Configure `ADVue-Theme-Pos` repository as a git submodule in theme path of your [ADempiere Vue](https://github.com/adempiere/adempiere-vue) workspace, and then track `develop` branch:
+
+   ```
+   git submodule add -b develop git@github.com:elsiosanchez/ADVue-Theme-Pos.git themes/pos
+   ```
+
+1. Fetch all the data:
+
+   ```
+   git submodule update --init --remote
+   ```
+1. Generate `local.json` file from script `generate-local-config.js`:
+   ```
+   node themes/pos/scripts/generate-local-config.js
+   ```
+1. Update [ADempiere Vue](https://github.com/adempiere/adempiere-vue) configuration by copying `local.json` file from `src/themes/pos` to root `config` directory.
+
+1. Download all dependencies and start development server:
+
+   ```
+   lerna bootstrap && yarn dev
+   ```
+
+1. That's it! Now, after opening your development server (http://localhost:9527 by pos), you should see [ADempiere Vue](https://github.com/adempiere/adempiere-vue) with the ADVue Theme Pos theme. 🎉
+
+## Design customization
+
+Si quieres personalizar los diseños de ADVue Theme Pos puedes empezar con este increíble archivo by Aditya Patel from HotWax Commerce.
---
+++ scripts/generate-local-config.js
@@ -0,0 +1,80 @@
+const { sh } = require('tasksfile')
+const config = require('../../../../config/default.json')
+const localConfig = require('../../../../config/local.json')
+const fs = require('fs');
+
+// List all the filenames before renaming
+
+getCurrentFilenames();
+
+// Rename the file
+
+fs.rename('config/default.json', 'config/local.json', (error) => {
+if (error) {
+	// Show the error
+	console.log(error);
+}
+else {
+	// List all the filenames after renaming
+	console.log("\nFile Renamed\n");
+	// List all the filenames after renaming
+	getCurrentFilenames();
+}
+});
+
+fs.rename('themes/pos/default.json', 'config/default.json', (error) => {
+  if (error) {
+    // Show the error
+    console.log(error);
+  }
+  else {
+    // List all the filenames after renaming
+    console.log("\nFile Renamed\n");
+    // List all the filenames after renaming
+    getCurrentFilenames();
+  }
+});
+
+// Function to get current filenames
+// in directory
+function getCurrentFilenames() {
+  console.log("Current filenames:");
+  fs.readdirSync(__dirname).forEach(file => {
+	  console.log(file);
+  });
+}
+
+/**
+ * Change view directory ADempiere
+ */
+// Change directory in the Browser
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Browser/index.vue`)
+// Change directory in the Form
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Form/index.vue`)
+// Change directory in the ImportExcel
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/ImportExcel/index.vue`)
+// Change directory in the Process
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Process/index.vue`)
+// Change directory in the ProcessActivity
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/ProcessActivity/index.vue`)
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/ProcessActivity/processActivity.js`)
+// Change directory in the Report
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Report/index.vue`)
+// Change directory in the ReportViewer
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/ReportViewer/index.vue`)
+// Change directory in the Summary
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Summary/index.vue`)
+// Change directory in the Workflow
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Workflow/index.vue`)
+// Change directory in the Test
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Test/index.vue`)
+// Change directory in the window
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Window/index.vue`)
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Window/MultiTabWindow.vue`)
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Window/windowLogicComponent.vue`)
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/ADempiere/Window/StandardWindow.vue`)
+/**
+ * Change view directory dashboard
+ */
+// Change directory in the Test
+sh(`sed -i "s|/${localConfig.theme}/|${config.theme}|g" src/views/dashboard/admin/index.vue`)
