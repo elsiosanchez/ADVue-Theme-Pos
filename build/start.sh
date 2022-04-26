9ee0b9312b68180f0137dc97cb9031d056b79c5e
elsiosanchez <elsiosanches@gmail.com>
2022-04-26 10:48:47 -0400

    add build

---
+++ build/start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# @autor Elsio Sanchez <elsiosanches@gmail.com>
+# Set enviroment variables to config file default.json and ecosystem.json
+
+# Set env values to proxy api rest ful
+sed -i "s|SERVER_HOST|$(hostname)|g"  $REPOSITORY_PATH/config/default.json
+sed -i "s|SERVER_PORT|$SERVER_PORT|g"  $REPOSITORY_PATH/config/default.json
+sed -i "s|API_URL|$API_URL|g"  $REPOSITORY_PATH/config/default.json
+sed -i "s|STORE_INDEX|$STORE_INDEX|g"  $REPOSITORY_PATH/config/default.json
+sed -i "s|MEMORY_RESTART|$MEMORY_RESTART|g"  $REPOSITORY_PATH/ecosystem.json
+sed -i "s|EXEC_INSTANCES|$EXEC_INSTANCES|g"  $REPOSITORY_PATH/ecosystem.json
+
+
+cd /var/www/adempiere-vue
+
+# Run app
+yarn run build && yarn run start && tail -f /dev/null
